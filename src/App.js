30022d9eacea9b847b11889efc958d1483d7128b
Bharatrathod-81 <94593235+Bharatrathod-81@users.noreply.github.com>
2021-12-10 23:16:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+import React from "react";
+import { useState } from "react";
+
+export default function App() {
+  var [show, setshow] = useState("");
+  var [raw, setraw] = useState("");
+
+  function change(event) {
+    setshow(event.target.value);
+  }
+  function click1(color) {
+    setraw(color);
+  }
+  return (
+    <div className="App">
+      <input onChange={change}></input>
+      <button onClick={() => click1("red")}>red</button>
+      <button onClick={() => click1("blue")}>blue</button>
+      <button id="rr" onClick={() => click1("green")}>
+        green
+      </button>
+      <p style={{ color: raw }}>{show}</p>
+    </div>
+  );
+}
